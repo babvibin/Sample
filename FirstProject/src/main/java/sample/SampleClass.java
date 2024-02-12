5eff30347dabf15ce03ec5cd0ae2bf9fd6895846
user <user@192.168.1.36>
2024-02-12 20:27:24 +0530

    SAmple Git Example

---
+++ FirstProject/src/main/java/sample/SampleClass.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		System.out.println(i);
+		
+		
+
+	}
+
+}
