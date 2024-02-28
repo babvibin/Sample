5d845ccaf9e1f0cf4dbb1747ba4b29028f98fafd
user <user@192.168.1.36>
2024-02-28 19:58:52 +0530

    test

--- FirstProject/src/main/java/interfaceEx/Sample1.java
+++ FirstProject/src/main/java/interfaceEx/Sample1.java
@@ -1,18 +1,19 @@
 package interfaceEx;
 
 public class Sample1 implements Sample {
+	
+	
+	
 	public static void main(String args[])
 	{
 		Sample1 obj=new Sample1();
-		obj.display();
-		
+		obj.display();		
 	}
 
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
 		System.out.println(" Test");
-
 	}
 
 }
